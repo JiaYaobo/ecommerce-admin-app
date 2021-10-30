5f43a0b6f5ce048622379d59d5bcf3cbf3bb14ce
jiayaobo <ybjia@ruc.edu.cn>
2021-10-30 23:55:28 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+import Home from "./pages/Home";
+
+
+const Container = styled.div`
+display: flex;
+margin: 10px;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Topbar/>
+      <Container>
+        <Sidebar/>
+        <Home/>
+      </Container>
+    </>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import Chart from "../components/Chart";
+import FeaturedInfo from "../components/FeaturedInfo";
+import { userData } from "../dummyData";
+
+const Container = styled.div`
+  flex: 4;
+`;
+
+const Home = () => {
+  return (
+    <Container>
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+    </Container>
+  );
+};
+
+export default Home;
